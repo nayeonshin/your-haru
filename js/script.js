7df9ae38e76cabe4a13b817b490b97034d4c9bf4
Nayeon Shin <dev.nayeon.shin@gmail.com>
2021-11-26 19:38:14 +0900

    feat: Add and remove .hidden

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function handleLogInSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+
+  greeting.innerText = `Hello ${username}!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", handleLogInSubmit);

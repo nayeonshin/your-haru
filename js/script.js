1e7cdfaee4d19f6a9219b082c677d2c79787b6a2
Nayeon Shin <dev.nayeon.shin@gmail.com>
2021-11-26 22:27:04 +0900

    feat: Hide form when username exists in storage

--- js/script.js
+++ js/script.js
@@ -3,15 +3,27 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function showGreeting(username) {
+  greeting.innerText = `Hello ${username}!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 function handleLogInSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
 
   const username = loginInput.value;
-  localStorage.setItem("username", username);
-  greeting.innerText = `Hello ${username}!`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+  showGreeting(username);
 }
 
-loginForm.addEventListener("submit", handleLogInSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", handleLogInSubmit);
+} else {
+  showGreeting(savedUsername);
+}

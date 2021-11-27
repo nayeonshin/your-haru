e98c6d268405e95c3c55b43e33a2729ea0f5f399
Nayeon Shin <dev.nayeon.shin@gmail.com>
2021-11-28 03:39:42 +0900

    feat(js): Delete background.js

--- js/background.js
+++
@@ -1,9 +0,0 @@
-function loadImage() {
-  const image = new Image();
-  image.src = "https://source.unsplash.com/random/1920x1080";
-  image.onload = () => {
-    console.log("loaded!");
-  };
-}
-
-loadImage();

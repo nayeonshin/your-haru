9cb604e1f06f17caa67186d89a3454fb44296565
Nayeon Shin <dev.nayeon.shin@gmail.com>
2021-11-27 19:28:50 +0900

    feat: Add fade in animation

--- js/username.js
+++ js/username.js
@@ -4,17 +4,24 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector(".greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const APPEAR_CLASSNAME = "appear";
+const DISAPPEAR_CLASSNAME = "disappear";
 const USERNAME_KEY = "username";
 
 function showGreeting(username) {
-  greeting.innerText = `Hello ${username}!`;
+  greeting.innerText = `Hello, ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.classList.add(APPEAR_CLASSNAME);
 }
 
 function handleLogInSubmit(event) {
   event.preventDefault(); // Stops browser from refreshing
-  loginFormBackground.classList.add(HIDDEN_CLASSNAME);
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginFormBackground.classList.add(DISAPPEAR_CLASSNAME);
+  loginForm.classList.add(DISAPPEAR_CLASSNAME);
+  setTimeout(() => {
+    loginFormBackground.classList.add(HIDDEN_CLASSNAME);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+  }, 1000);
 
   const inputUsername = loginInput.value;
   localStorage.setItem(USERNAME_KEY, inputUsername);
@@ -25,7 +32,9 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   loginFormBackground.classList.remove(HIDDEN_CLASSNAME);
+  loginFormBackground.classList.add(APPEAR_CLASSNAME);
   loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.classList.remove(APPEAR_CLASSNAME);
   loginForm.addEventListener("submit", handleLogInSubmit);
 } else {
   showGreeting(savedUsername);

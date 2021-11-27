0c74bc01a0214201df13e2e03ee27f69b2fe9a81
Nayeon Shin <dev.nayeon.shin@gmail.com>
2021-11-27 12:50:09 +0900

    feat(css): Align login form in center

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const loginForm = document.querySelector("#login-form");
+const loginForm = document.querySelector(".login-form");
 const loginInput = loginForm.querySelector("input");
-const greeting = document.querySelector("#greeting");
+const greeting = document.querySelector(".greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";

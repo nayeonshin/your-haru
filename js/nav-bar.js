8e2aae5e4824664196f71a16974fe2f3310b4b10
Nayeon Shin <dev.nayeon.shin@gmail.com>
2021-11-28 23:07:28 +0900

    fix(js): onclick to click

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -3,7 +3,6 @@ const settingScreen = document.querySelector(".js-background__setting");
 
 const navigationBar = document.querySelector(".js-background__navigation");
 const homeButton = navigationBar.querySelector("li:first-child button");
-const settingButton = navigationBar.querySelector("li:last-child button");
 
 // let isOnHomeScreen = true;
 
@@ -11,10 +10,6 @@ function handleHomeBtnClick() {
   console.log("home clicked!");
 }
 
-function handleSettingBtnClick() {
-  console.log("setting clicked!");
-}
-
 console.log(homeButton);
-homeButton.addEventListener("onclick", handleHomeBtnClick);
-settingButton.addEventListener("onclick", handleSettingBtnClick);
+// console.dir(homeButton);
+homeButton.addEventListener("click", handleHomeBtnClick);

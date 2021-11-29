3709c8e073779d1fb31711cb6d94c98cae1443ff
Nayeon Shin <dev.nayeon.shin@gmail.com>
2021-11-30 01:09:21 +0900

    feat(time.js): Make currentClock item type consistent

--- js/time.js
+++ js/time.js
@@ -53,7 +53,7 @@ function updateClock(currentTime) {
   const isCurrentAm = currentHours < 12 ? true : false;
   const amPm = isCurrentAm ? "am" : "pm";
   if (currentHours === 0) {
-    currentClock[0] = 12; // When 12 a.m., displays 12
+    currentClock[0] = String(12); // When 12 a.m., displays 12
   } else if (!isCurrentAm) {
     currentClock[0] = String(currentHours - 12);
   }

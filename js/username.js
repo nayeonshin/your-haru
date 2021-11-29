08dc4dec45168f35fe22adadca1cccf03ca954b2
Nayeon Shin <dev.nayeon.shin@gmail.com>
2021-11-30 04:25:39 +0900

    feat(setting): Toggle switches *kinda* work lol

--- js/username.js
+++ js/username.js
@@ -6,7 +6,6 @@ const loginInput = loginForm.querySelector("input");
 
 const greeting = document.querySelector(".js-background__greeting");
 
-const INVISIBLE_CLASSNAME = "invisible";
 const USERNAME_KEY = "username";
 
 // TODO: If name length long (~50), change font size of greeting

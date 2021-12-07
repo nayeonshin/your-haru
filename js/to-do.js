f85e792809e1747db4f416204b169435300617d5
Nayeon Shin <dev.nayeon.shin@gmail.com>
2021-12-07 09:09:51 +0900

    feat(to-do.css): Make to-do screen responsive

--- js/to-do.js
+++ js/to-do.js
@@ -1,9 +1,16 @@
-const toDoForm = TO_DO_SCREEN.querySelector(".js-to-do__form");
+const toDoForm = TO_DO_SCREEN.querySelector(".js-to-do-list__form");
 const toDoInput = toDoForm.querySelector("input");
-const toDoList = TO_DO_SCREEN.querySelector(".js-to-do__list");
+const toDoList = TO_DO_SCREEN.querySelector(".js-to-do-list__list");
+
+// TODO: Add a check button -> strikethrough
 
 function showToDo(newToDo) {
-  console.log("I will show", newToDo);
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+
+  li.appendChild(span);
+  span.innerText = newToDo;
+  toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event) {

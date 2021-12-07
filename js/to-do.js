a603cba933cbb0220cfbf4ccb4ace723469eef28
Nayeon Shin <dev.nayeon.shin@gmail.com>
2021-12-08 02:36:16 +0900

    feat(css): Add more styles to to-do screen

--- js/to-do.js
+++ js/to-do.js
@@ -5,6 +5,7 @@ const toDoList = TO_DO_SCREEN.querySelector(".js-to-do-list__list");
 
 // TODO: Add a check button -> strikethrough
 // TODO: Calendar
+// TODO: If todo input is long, insert line breaks? (or put limit to char count)
 
 function deleteTodo(event) {
   const toDo = event.target.parentElement;
@@ -15,19 +16,19 @@ function showToDo(newToDo) {
   const li = document.createElement("li");
   li.classList.add("list__to-do");
 
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-
   const deleteButton = document.createElement("input");
   deleteButton.type = "button";
 
   deleteButton.addEventListener(CLICK_EVENT, deleteTodo);
 
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
   const span = document.createElement("span");
   span.innerText = newToDo;
 
-  li.appendChild(checkbox);
   li.appendChild(deleteButton);
+  li.appendChild(checkbox);
   li.appendChild(span);
 
   toDoList.appendChild(li);
